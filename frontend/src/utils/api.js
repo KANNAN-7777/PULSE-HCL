6b3f207d07f0168d2776dc4aceade3bcbceb81ce
kannan <kannan915923@gmail.com>
2026-09-19 13:08:55 +0530

    Fix frontend API URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,10 +1,7 @@
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://localhost:8080/api",
+  baseURL: "http://192.168.0.104:8080/api",
 });
 
 api.interceptors.request.use(
